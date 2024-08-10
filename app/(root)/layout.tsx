617d483b2a0bd1a3e1d04302643a5ceb568a09f3
adilavirk <adilavirk0@gmail.com>
2024-08-10 17:22:11 +0500

    complete mobile and desktop navbar

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,12 +1,26 @@
+import MobileNavbar from "@/components/MobileNavbar";
+import SideBar from "@/components/SideBar";
+import Image from "next/image";
+
 export default async function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const loggedIn = { firstName: "Adila", lastName: "Arshad" };
   return (
-    <main>
-      SIDEBAR
-      {children}
+    <main className="flex h-screen w-full font-inter">
+      <SideBar user={loggedIn} />
+      {/* Mobile Nav Bar */}
+      <div className="flex size-full flex-col">
+        <div className="root-layout">
+          <Image src="/icons/logo.svg" width={30} height={30} alt="logo" />
+          <div>
+            <MobileNavbar user={loggedIn} />
+          </div>
+        </div>
+        {children}
+      </div>
     </main>
   );
 }
